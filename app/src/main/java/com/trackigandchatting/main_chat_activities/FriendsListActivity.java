b153aa1b8d3e3d64b2130834c7384ad40d699872
kalin <kalindusupun@gmail.com>
2021-11-02 11:47:58 +0530

    Trackig-And-Chatting

--- app/src/main/java/com/trackigandchatting/main_chat_activities/FriendsListActivity.java
+++ app/src/main/java/com/trackigandchatting/main_chat_activities/FriendsListActivity.java
@@ -1,13 +1,30 @@
 package com.trackigandchatting.main_chat_activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -15,12 +32,14 @@ import com.trackigandchatting.R;
 import com.trackigandchatting.chat_adapters.AllFriendsAdapter;
 import com.trackigandchatting.models.ChatsModel;
 
-public class FriendsListActivity extends AppCompatActivity {
+public class FriendsListActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firebaseFirestore;
     RecyclerView recyclerViewChat;
     AllFriendsAdapter allChatsAdapter;
+    GoogleMap googleMap;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +51,95 @@ public class FriendsListActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
         recyclerViewChat = findViewById(R.id.recyclerViewFriendList);
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(FriendsListActivity.this);
+        showMap();
         syncChatPeopleFromFirestore();
     }
+
+    private void showMap() {
+
+        // if you use mapfragment then lifecycle methods are not needed
+        //otherwise map will not display
+        // https://developers.google.com/maps/documentation/android-sdk/reference/com/google/android/libraries/maps/MapView?hl=en
+        //https://developers.google.com/maps/documentation/android-sdk/reference/com/google/android/libraries/maps/MapFragment?hl=en
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.user_list_map);
+        mapFragment.getMapAsync(this); //onMapReady method automatically call. your Default map
+
+    }
+
+    public void markOnMap(double latitude, double longitude, float zoomLevel, String locationName, String snippet) {
+
+        googleMap.clear(); // clear map and remove markers
+
+        LatLng latLng = new LatLng(latitude, longitude);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title(locationName);
+        markerOptions.position(latLng);
+        markerOptions.snippet(snippet); //place info's
+        // googleMap.addMarker(markerOptions).remove();
+        googleMap.addMarker(markerOptions);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel);  //max zoom 21. 1world, 5Continents, 10Cities, 15Streets, 20Buildings
+        //googleMap.moveCamera(cameraUpdate); //directly show
+        // googleMap.animateCamera(cameraUpdate); // moving to position without time
+
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(2.0f));
+        googleMap.animateCamera(cameraUpdate, 3000, new GoogleMap.CancelableCallback() { // moving to position with time
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+        });
+    }
+
+    public void getLastKnowLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+
+                Location location = (Location) task.getResult();
+                Toast.makeText(FriendsListActivity.this, "Latitude1: " + location.getLatitude() + " Longitude1: " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+                markOnMap(location.getLatitude(), location.getLongitude(), 15,"My Location","Address: Nugegoda\nPhone Number: +94777");
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap=googleMap;
+       // markOnMap(6.8649, 79.8997, 15,"My Location","Address: Nugegoda\nPhone Number: +94777");
+        getLastKnowLocation();
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true); // Compass not showing until you rotate the map
+
+        // googleMap.getUiSettings().setZoomGesturesEnabled(false); // map cant zoom
+        googleMap.getUiSettings().setZoomGesturesEnabled(true);
+
+        //googleMap.getUiSettings().setScrollGesturesEnabled(false);// map cant Scroll
+        googleMap.getUiSettings().setScrollGesturesEnabled(true);// map cant Scroll
+
+        //googleMap.getUiSettings().setRotateGesturesEnabled(false);// map cant Rotate
+        googleMap.getUiSettings().setRotateGesturesEnabled(true);
+        googleMap.getUiSettings().setTiltGesturesEnabled(true);
+        googleMap.getUiSettings().setMapToolbarEnabled(true); // when you tap on marker, shows open from google map and direction
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+    }
+
 
     private void syncChatPeopleFromFirestore() {
 
--- app/src/main/java/com/trackigandchatting/user_login/VerifyUserMobileNumber.java
+++ app/src/main/java/com/trackigandchatting/user_login/VerifyUserMobileNumber.java
@@ -3,8 +3,13 @@ package com.trackigandchatting.user_login;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -12,12 +17,20 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.hbb20.CountryCodePicker;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.trackigandchatting.R;
 
 import java.util.concurrent.TimeUnit;
@@ -33,6 +46,7 @@ public class VerifyUserMobileNumber extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
     String codeSent;
+    boolean checkGpsPermission=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +62,8 @@ public class VerifyUserMobileNumber extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
 
         countryCode = countrycodepicker.getSelectedCountryCodeWithPlus();
+
+        checkPermissions();
 
         countrycodepicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
             @Override
@@ -76,6 +92,11 @@ public class VerifyUserMobileNumber extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Please enter your number", Toast.LENGTH_SHORT).show();
                 } else if (number.length() < 9) {
                     Toast.makeText(getApplicationContext(), "Please enter correct number", Toast.LENGTH_SHORT).show();
+                }
+                else if(checkGpsPermission==false){
+                    Toast.makeText(getApplicationContext(), "Please enable Location permission", Toast.LENGTH_SHORT).show();
+                    checkPermissions();
+
                 }else{
 
                     progressbarofmain.setVisibility(View.VISIBLE);
@@ -125,6 +146,62 @@ public class VerifyUserMobileNumber extends AppCompatActivity {
                 finish();
             }
         };
+    }
+
+    public void checkPermissions() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                //  Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+
+                //google map required Google play services in the phone
+                if (checkGooglePlayServices()) {
+                    Toast.makeText(VerifyUserMobileNumber.this, "Google play services Available", Toast.LENGTH_SHORT).show();
+                    checkGpsPermission=true;
+
+                } else {
+                    Toast.makeText(VerifyUserMobileNumber.this, "Google play services NOT  Available", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), "");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    private boolean checkGooglePlayServices() {
+
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int result = googleApiAvailability.isGooglePlayServicesAvailable(this);
+
+        if (result == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (googleApiAvailability.isUserResolvableError(result)) {
+            Dialog dialog = googleApiAvailability.getErrorDialog(this, result, 201, new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialogInterface) {
+                    Toast.makeText(VerifyUserMobileNumber.this, "User Cancelled Dialog", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            dialog.show();
+        }
+
+        return false;
     }
 
 }
